a4abf9fc9b57a5780fd87bcd6e3d0c2e5d0fdd84
Digitalgremlin <JSlade@flammabletoys.com>
2014-08-24 19:13:21 -0700

    add image render option

--- test/app/scripts/views/slide.js
+++ test/app/scripts/views/slide.js
@@ -5,10 +5,20 @@ define([
 		className: 'slide',
 
 		render: function() {
+			if ( this.model.get('image')) {
+				this.renderImage();
+			}
+
 			this.$el.append(
 				'<h1 class="' + this.model.get('size') + '">' + this.model.get('title') + '</h1>'
 				);
 			return this;
+		},
+
+		renderImage: function() {
+			this.$el
+			.addClass('image')
+			.append('<img src="' +  this.model.get('image') + '">');
 		}
 
 	});
